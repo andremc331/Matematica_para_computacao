ea82708b6e08228c290df2043bd71397fff79705
andremc331 <andremc331@gmail.com>
2024-10-01 20:18:03 -0300

    EP2.1 e 2.2

---
+++ semana02-10/exerc2.1/DiasDaSemana.ts
@@ -0,0 +1,9 @@
+export enum DiasDaSemana {
+    Domingo = 0,
+    Segunda,
+    Terça,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sábado
+  }
---
+++ semana02-10/exerc2.1/MesesDoAno.ts
@@ -0,0 +1,16 @@
+export enum MesesDoAno {
+    Janeiro = 1,
+    Fevereiro,
+    Março,
+    Abril,
+    Maio,
+    Junho,
+    Julho,
+    Agosto,
+    Setembro,
+    Outubro,
+    Novembro,
+    Dezembro
+  }
+
+  //aqui começa a enumeração de meses
