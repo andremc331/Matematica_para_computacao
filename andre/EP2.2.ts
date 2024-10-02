570d29c6e3817ccf37e07a42fd348940d8474b01
andremc331 <151594451+andremc331@users.noreply.github.com>
2024-10-02 18:23:10 -0300

    Update EP2.2.ts

--- andre/EP2.2.ts
+++ andre/EP2.2.ts
@@ -7,3 +7,19 @@ enum CursosGraduacao {
   // Exemplo de uso:
   const meuCurso: CursosGraduacao = CursosGraduacao.DesenvolvimentoDeSoftwareMultiplataforma;
   console.log(meuCurso); // Saída: Desenvolvimento de Software Multiplataforma
+
+
+  AnáliseEDesenvolvimentoDeSistemas = 1,
+  GestãoDaTecnologiaDaInformação,
+  EngenhariaDaComputação,
+  Logística,
+  ProduçãoIndustrial,
+  ManufaturaAvançada,
+  Marketing,
+  GestãoEmpresarial
+}
+
+// Exemplo de uso:
+const meuCurso: CursosGraduacao = CursosGraduacao.AnáliseEDesenvolvimentoDeSistemas;
+console.log(`Curso selecionado: ${meuCurso} - ${CursosGraduacao[meuCurso]}`);
+// Saída: Curso selecionado: 1 - AnáliseEDesenvolvimentoDeSistemas
