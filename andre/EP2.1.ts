731de7f9611a801df9059c62950eac561200a733
andremc331 <151594451+andremc331@users.noreply.github.com>
2024-10-02 16:59:53 -0300

    Update EP2.1.ts

--- andre/EP2.1.ts
+++ andre/EP2.1.ts
@@ -1,10 +1,13 @@
-enum ParentescoDireto {
-    Pai = "Pai",
-    Mãe = "Mãe",
-    IrmãoDoMeio = "Irmão do Meio",
-    IrmãoMaisVelho = "Irmão Mais Velho",
-  }
-  
-  // Exemplo de uso:
-  const meuParente: ParentescoDireto = ParentescoDireto.Pai;
-  console.log(meuParente); // Saída: Pai
+enum MembrosDaFamilia {
+  Pai = 1,
+  Mãe,
+  IrmãoMaisVelho,
+  IrmãoDoMeio,
+  IrmãMaisNova,
+  FilhoMaisVelho,
+  FilhoMaisNovo,
+}
+
+// Exemplo de uso:
+const meuParente: MembrosDaFamilia = MembrosDaFamilia.Mãe;
+console.log(`O número correspondente à Mãe é: ${meuParente}`); // Saída: O número correspondente à Mãe é: 2
