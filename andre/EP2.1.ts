06fcb27acf246b33f13d4a5383cb50ac238e8dbc
andremc331 <andremc331@gmail.com>
2024-10-01 22:21:24 -0300

    Main

---
+++ andre/EP2.1.ts
@@ -0,0 +1,10 @@
+enum ParentescoDireto {
+    Pai = "Pai",
+    Mãe = "Mãe",
+    IrmãoDoMeio = "Irmão do Meio",
+    IrmãoMaisVelho = "Irmão Mais Velho",
+  }
+  
+  // Exemplo de uso:
+  const meuParente: ParentescoDireto = ParentescoDireto.Pai;
+  console.log(meuParente); // Saída: Pai
---
+++ andre/EP2.2.ts
@@ -0,0 +1,9 @@
+enum CursosGraduacao {
+    DesenvolvimentoDeSoftwareMultiplataforma = "Desenvolvimento de Software Multiplataforma",
+    MeioAmbienteERecursosHídricos = "Meio Ambiente e Recursos Hídricos",
+    Geoprocessamento = "Geoprocessamento",
+  }
+  
+  // Exemplo de uso:
+  const meuCurso: CursosGraduacao = CursosGraduacao.DesenvolvimentoDeSoftwareMultiplataforma;
+  console.log(meuCurso); // Saída: Desenvolvimento de Software Multiplataforma
